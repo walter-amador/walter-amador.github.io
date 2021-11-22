3db3849bcd971f5f3f7a69cf85ead14d8b5b0caf
WalterIran <amadorwalter3418@gmail.com>
2021-11-22 05:52:31 +0000

    Deploying to gh-pages from @ WalterIran/WalterIran.github.io@a6dbfb4381491bcf9fae2236088fc642b487a0f3 🚀

--- _next/static/Ed6aNaqeT--OTFC0mw7lv/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/78-800fd9d407430bd7a160.js","static/css/7bc38b8e7b34139accd8.css","static/chunks/pages/index-6f356a78d25ce8ee9d6e.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
