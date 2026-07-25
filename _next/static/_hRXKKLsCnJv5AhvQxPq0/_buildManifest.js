acc41476d4233fde0af567c9a7cb687f3794913b
walter-amador <amadorwalter3418@gmail.com>
2026-07-25 01:28:11 +0000

    Deploying to gh-pages from @ walter-amador/walter-amador.github.io@6b8f7d11e9b5b920d45d9e791cba0004ef84999f 🚀

--- _next/static/_hRXKKLsCnJv5AhvQxPq0/_buildManifest.js
+++ _next/static/_hRXKKLsCnJv5AhvQxPq0/_buildManifest.js
@@ -1,6 +1,6 @@
 self.__BUILD_MANIFEST = {
   "/": [
-    "static/chunks/ec8c8e2c47797800.js"
+    "static/chunks/41a4a00af7a56bb6.js"
   ],
   "/_error": [
     "static/chunks/ad66fb46ddb301f0.js"
--- _next/static/chunks/41a4a00af7a56bb6.js
+++ _next/static/chunks/41a4a00af7a56bb6.js
@@ -1,6 +1,6 @@
 __turbopack_load_page_chunks__("/", [
   "static/chunks/5b97997cf8d4866e.js",
   "static/chunks/e0d3dbca2e08ee17.js",
-  "static/chunks/d7258c28f5775002.js",
-  "static/chunks/turbopack-98d7462f501b0cf5.js"
+  "static/chunks/352539482cab3b22.js",
+  "static/chunks/turbopack-a6136a197af2d5d1.js"
 ])
